9fe783fa6b289016beb08c1c3c4ce2d69236a361
TheBrenny <iam+dev@justbrenny.me>
2021-09-22 18:46:40 +1000

    run npm

--- configure.js
+++ configure.js
@@ -21,7 +21,7 @@ if(types.includes(type) === false) {
 const pwd = fs.realpathSync(path.resolve(__dirname, "."));
 fs.renameSync(path.join(pwd, `package-${type}.json`), path.join(pwd, "package.json"));
 fs.renameSync(path.join(pwd, `app-${type}`), path.join(pwd, "app"));
-fs.renameSync(path.join(pwd, `server-${type}.js`), path.join(pwd, "server"));
+fs.renameSync(path.join(pwd, `server-${type}.js`), path.join(pwd, "server.js"));
 
 let rmFiles = [
     "package-#.json",
@@ -40,10 +40,28 @@ types.forEach(t => {
     });
 });
 
-cp.spawnSync("npm", ["update", "--save-dev"], {stdio: "inherit"});
-cp.spawnSync("npm", ["install", "--save-dev"], {stdio: "inherit"});
 
-console.log("Done.");
+Promise.resolve()
+    .then(() => new Promise((resolve, reject) => {
+        let proc = cp.spawn("npm", ["update", "--save-dev"], {
+            shell: true,
+            windowsHide: true,
+        });
+        proc.stdout.on("data", data => console.log(data.toString()));
+        proc.stderr.on("data", data => console.log(data.toString()));
+        proc.on("close", code => console.log("npm finished with code " + code));
+    }))
+    .then(() => new Promise((resolve, reject) => {
+        let proc = cp.spawn("npm", ["install", "--save-dev"], {
+            shell: true,
+            windowsHide: true,
+        });
+        proc.stdout.on("data", data => console.log(data.toString()));
+        proc.stderr.on("data", data => console.log(data.toString()));
+        proc.on("close", code => console.log("npm finished with code " + code));
+    }))
+    .then(() => fs.rmSync(path.join(pwd, "configure.js"), {recursive: true, force: true}))
+    .then(() => console.log("Done!"));
 
 function printUsage() {
     console.log(`Usage: node configure.js <${types.join("|")}>`);
