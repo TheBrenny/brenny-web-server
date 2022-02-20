3e67a81f2fea4c2a2453baabe94f99ed11dc85f1
Jarod Brennfleck <robinjars@gmail.com>
2022-02-20 22:50:30 +1100

    Update configure.js
    no testing was done.
    
    this was done using github.
    
    don't h8

--- configure.js
+++ configure.js
@@ -18,57 +18,83 @@ if(types.includes(type) === false) {
     return;
 }
 
+console.log("Cheers for choosing BWS! - Making server: " + type);
+let startTime = performance.now();
+
+process.stdout.write("> Renaming files and folders... ");
 const pwd = fs.realpathSync(path.resolve(__dirname, "."));
 fs.renameSync(path.join(pwd, `package-${type}.json`), path.join(pwd, "package.json"));
 fs.renameSync(path.join(pwd, `app-${type}`), path.join(pwd, "app"));
 fs.renameSync(path.join(pwd, `server-${type}.js`), path.join(pwd, "server.js"));
+process.stdout.write("Done!\n");
 
+process.stdout.write("> Removing redundant files and folders...\n");
 let rmFiles = [
     "package-#.json",
     "app-#/",
     "server-#.js",
 ];
-
 types.forEach(t => {
     rmFiles.forEach(f => {
         f = f.replace("#", t);
         try {
+            process.stdout.write(`  > Removing ${f}... `);
             fs.rmSync(path.join(pwd, f), {recursive: true, force: true});
+            process.stdout.write(`Done\n`);
         } catch(e) {
-            console.warn(`Failed to remove ${f}. Do this manually.`);
+            process.stderr.write(`Error!\n    ! Failed to remove ${f}. Do this manually.\n`);
         }
     });
 });
+process.stdout.write(`Done!\n`);
 
 Promise.resolve()
     .then(() => new Promise((resolve, reject) => {
-        let proc = cp.spawn("npm", ["update", "--save-dev"], {
+        process.stdout.write(`> Installing dependencies... `);
+        let proc = cp.spawn("npm", ["install", "--save", "--save-dev"], {
             shell: true,
             windowsHide: true,
         });
-        proc.stdout.on("data", data => console.log(data.toString()));
-        proc.stderr.on("data", data => console.log(data.toString()));
+        //proc.stdout.on("data", data => console.log(data.toString()));
+        //proc.stderr.on("data", data => console.log(data.toString()));
         proc.on("close", code => {
-            console.log("npm finished with code " + code);
-            if(code === 0) resolve();
-            else reject(code);
+            //console.log("npm finished with code " + code);
+            if(code === 0) {
+                process.stdout.write(`Done!\n`);
+                resolve();
+            } else {
+                process.stderr.write(`Error!\n  ! Failed to install dependencies - run 'npm i' and diagnose.\n  ! Proceeding anyway...\n`);
+                reject(code);
+            }
         });
     }))
     .then(() => new Promise((resolve, reject) => {
-        let proc = cp.spawn("npm", ["install", "--save-dev"], {
+        process.stdout.write(`> Updating dependencies to the latest versions... `);
+        let proc = cp.spawn("npm", ["update", "--save", "--save-dev"], {
             shell: true,
             windowsHide: true,
         });
-        proc.stdout.on("data", data => console.log(data.toString()));
-        proc.stderr.on("data", data => console.log(data.toString()));
+        //proc.stdout.on("data", data => console.log(data.toString()));
+        //proc.stderr.on("data", data => console.log(data.toString()));
         proc.on("close", code => {
-            console.log("npm finished with code " + code);
-            if(code === 0) resolve();
-            else reject(code);
+            //console.log("npm finished with code " + code);
+            if(code === 0) {
+                process.stdout.write(`Done!\n`);
+                resolve();
+            } else {
+                process.stderr.write(`Error!\n  ! Failed to update dependencies - run 'npm i' and diagnose.\n  ! Proceeding anyway...\n`);
+                reject(code);
+            }
         });
     }))
-    .then(() => fs.rmSync(path.join(pwd, "configure.js"), {recursive: true, force: true}))
-    .then(() => console.log("Done!"));
+    .then(() => {
+        process.stdout.write(`> Deleting configure script... `);
+        fs.rmSync(path.join(pwd, "configure.js"), {recursive: true, force: true}
+        process.stdout.write(`Done!\n`);
+    }))
+    .then(() => {
+        process.stdout.write(`All done!`);
+    });
 
 function printUsage() {
     console.log(`Usage: node configure.js <${types.join("|")}>`);
