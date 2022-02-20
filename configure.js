91fd8ddaf6c9c54d2dc814f09099c17e9ad527fa
Jarod Brennfleck <robinjars@gmail.com>
2022-02-20 23:11:53 +1100

    ansi codes are cool!

--- configure.js
+++ configure.js
@@ -21,32 +21,35 @@ if(types.includes(type) === false) {
 console.log("Cheers for choosing BWS! - Making server: " + type);
 let startTime = performance.now();
 
-process.stdout.write("> Renaming files and folders... ");
+process.stdout.write("> Renaming files and folders...");
 const pwd = fs.realpathSync(path.resolve(__dirname, "."));
 fs.renameSync(path.join(pwd, `package-${type}.json`), path.join(pwd, "package.json"));
 fs.renameSync(path.join(pwd, `app-${type}`), path.join(pwd, "app"));
 fs.renameSync(path.join(pwd, `server-${type}.js`), path.join(pwd, "server.js"));
 process.stdout.write("Done!\n");
 
-process.stdout.write("> Removing redundant files and folders...\n");
+process.stdout.write("> Removing redundant files and folders...\0337\n");
 let rmFiles = [
     "package-#.json",
     "app-#/",
     "server-#.js",
 ];
+let lineCount = 0;
 types.forEach(t => {
     rmFiles.forEach(f => {
         f = f.replace("#", t);
         try {
             process.stdout.write(`  > Removing ${f}... `);
             fs.rmSync(path.join(pwd, f), {recursive: true, force: true});
-            process.stdout.write(`Done\n`);
+            process.stdout.write(`Done!\n`);
         } catch(e) {
-            process.stderr.write(`Error!\n    ! Failed to remove ${f}. Do this manually.\n`);
+            process.stderr.write(`\033[31mError!\n    ! Failed to remove ${f}. Do this manually.\033[0m\n`);
+            lineCount += 1; // bc we print on a new line!
         }
+        lineCount += 1; // bc we're adding a line anyway
     });
 });
-process.stdout.write(`Done!\n`);
+process.stdout.write(`\0338\033[${lineCount}ADone!\033[${lineCount}B\033[0G`);
 
 Promise.resolve()
     .then(() => new Promise((resolve, reject) => {
@@ -63,7 +66,7 @@ Promise.resolve()
                 process.stdout.write(`Done!\n`);
                 resolve();
             } else {
-                process.stderr.write(`Error!\n  ! Failed to install dependencies - run 'npm i' and diagnose.\n  ! Proceeding anyway...\n`);
+                process.stderr.write(`\033[31mError!\n  ! Failed to install dependencies - run 'npm i' and diagnose.\n  ! Proceeding anyway...\033[0m\n`);
                 reject(code);
             }
         });
@@ -82,18 +85,18 @@ Promise.resolve()
                 process.stdout.write(`Done!\n`);
                 resolve();
             } else {
-                process.stderr.write(`Error!\n  ! Failed to update dependencies - run 'npm i' and diagnose.\n  ! Proceeding anyway...\n`);
+                process.stderr.write(`\033[31mError!\n  ! Failed to update dependencies - run 'npm i' and diagnose.\n  ! Proceeding anyway...\033[0m\n`);
                 reject(code);
             }
         });
     }))
     .then(() => {
-        process.stdout.write(`> Deleting configure script... `);
+        process.stdout.write(`> Deleting configure script...`);
         fs.rmSync(path.join(pwd, "configure.js"), {recursive: true, force: true});
         process.stdout.write(`Done!\n`);
     })
     .then(() => {
-        process.stdout.write(`All done!`);
+        process.stdout.write(`All done!\n`);
     });
 
 function printUsage() {
