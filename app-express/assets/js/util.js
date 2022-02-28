b9b53ca8adb4aa469b7178e19578ed39dd9c3aff
Jarod Brennfleck <robinjars@gmail.com>
2022-03-01 00:31:49 +1100

    updated with more fns

--- app-express/assets/js/util.js
+++ app-express/assets/js/util.js
@@ -5,3 +5,61 @@ function $(selector) {
 function $$(selector) {
     return document.querySelectorAll(selector);
 }
+
+Object.defineProperty(HTMLElement.prototype, "$", {
+    value: function (selector) {
+        return this.querySelector(selector);
+    }
+});
+
+Object.defineProperty(HTMLElement.prototype, "$$", {
+    value: function (selector) {
+        return this.querySelectorAll(selector);
+    }
+});
+
+Object.defineProperty(HTMLElement.prototype, "$up", {
+    value: function () {
+        if(this.parentElement) {
+            if(this.parentElement.matches(...arguments)) return this.parentElement;
+            return this.parentElement.$up(...arguments);
+        } else {
+            return undefined;
+        }
+    }
+});
+
+Object.defineProperty(HTMLElement.prototype, "$$up", {
+    value: function (list) {
+        if(list === undefined || list === null) list = [];
+
+        if(this.parentElement) {
+            if(this.parentElement.matches(...arguments)) list.push(this.parentElement);
+            return this.parentElement.$$up(...arguments);
+        } else {
+            return Reflect.construct(Array, list, NodeList);
+        }
+    }
+});
+
+/**
+ * Adds an event listener for the window.load event.
+ * 
+ * Window.load occurs when the whole page is loaded (including style sheets, images, iframes, etc.).
+ */
+function onLoad(fn) {
+    window.addEventListener('load', fn);
+}
+
+/**
+ * Adds an event listener for the document.DOMContentLoaded event.
+ * 
+ * Document.DOMContentLoaded occurs when the page has been loaded (EXCLUDING the parsing of style sheets, images, iframes, etc.).
+ */
+function onReady(fn) {
+    if(document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
